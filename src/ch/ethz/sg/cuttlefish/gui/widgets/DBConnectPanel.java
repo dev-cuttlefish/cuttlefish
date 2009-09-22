4f3a34dcc61752268b1a907eaf8f0a733ed2a6f2
gadavid <gadavid@deb517fa-dd3d-0410-ac94-988ead23a7ee>
2009-09-22 16:38:23 +0000

    Database browsing 1.0

---
+++ src/ch/ethz/sg/cuttlefish/gui/widgets/DBConnectPanel.java
@@ -0,0 +1,151 @@
+/*
+  
+    Copyright (C) 2009  Markus Michael Geipel, David Garcia Becerra
+
+	This file is part of Cuttlefish.
+	
+ 	Cuttlefish is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+*/
+
+package ch.ethz.sg.cuttlefish.gui.widgets;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+import ch.ethz.sg.cuttlefish.gui.BrowserWidget;
+import ch.ethz.sg.cuttlefish.networks.DBNetwork;
+import ch.ethz.sg.cuttlefish.networks.StaticCxfNetwork;
+
+public class DBConnectPanel extends BrowserWidget {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JButton connectButton = null;
+	private JTextField urlField = null;
+	private JTextField userNameField = null;
+	private JPasswordField passwordField = null;
+	
+	/**
+	 * This method initializes 
+	 * 
+	 */
+	public DBConnectPanel() {
+		super();
+		initialize();
+		setNetworkClass(DBNetwork.class);
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 */
+	private void initialize() {
+		GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
+		gridBagConstraints1.fill = GridBagConstraints.VERTICAL;
+		gridBagConstraints1.gridy = 1;
+		gridBagConstraints1.weightx = 1.0;
+		gridBagConstraints1.insets = new Insets(2, 2, 2, 2);
+		gridBagConstraints1.gridx = 0;
+		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.insets = new Insets(2, 2, 2, 2);
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstraints.gridy = 2;
+
+		GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
+		gridBagConstraints2.gridx = 0;
+		gridBagConstraints2.insets = new Insets(2, 2, 2, 2);
+		gridBagConstraints2.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstraints2.gridy = 3;
+
+		GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
+		gridBagConstraints3.gridx = 0;
+		gridBagConstraints3.insets = new Insets(2, 2, 2, 2);
+		gridBagConstraints3.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstraints3.gridy = 4;
+
+		this.setLayout(new GridBagLayout());
+        this.add(getConnectButton(), gridBagConstraints1);
+        this.add(getUrlField(), gridBagConstraints);
+        this.add(getUserNameField(), gridBagConstraints2);
+        this.add(getPasswordField(), gridBagConstraints3);
+ 	}
+
+	private JTextField getUrlField() {
+		if (urlField == null)
+		{
+			urlField = new JTextField("Database URL");
+			urlField.setToolTipText("Database URL");
+		}
+		return urlField;
+	}
+
+	private JTextField getUserNameField() {
+		if (userNameField == null)
+		{
+			userNameField = new JTextField("User name");
+			userNameField.setToolTipText("User name");
+		}
+		return userNameField;
+	}
+
+	private JTextField getPasswordField() {
+		if (passwordField == null)
+		{
+			passwordField = new JPasswordField("Password");
+			passwordField.setToolTipText("Password");
+		}
+		return passwordField;
+	}
+
+	
+	/**
+	 * This method initializes stopButton	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getConnectButton() {
+		if (connectButton == null) {
+			connectButton = new JButton();
+			connectButton.setText("Connect");
+			connectButton.setEnabled(true);
+			connectButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+				   ((DBNetwork) getNetwork()).connect(getUrlField().getText(),
+						   getUserNameField().getText(), getPasswordField().getText());
+				}
+			});
+		}
+		return connectButton;
+	}
+
+	@Override
+	public void init() {
+	}
+
+}
