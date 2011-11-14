59f33940850fa62ec1fcea2557876bd4891d3667
tsankovp <tsankovp@deb517fa-dd3d-0410-ac94-988ead23a7ee>
2011-11-14 16:48:57 +0000

    interactive svg finished

--- src/ch/ethz/sg/cuttlefish/misc/SVGExporter.java
+++ src/ch/ethz/sg/cuttlefish/misc/SVGExporter.java
@@ -1,10 +1,13 @@
 package ch.ethz.sg.cuttlefish.misc;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import ch.ethz.sg.cuttlefish.networks.BrowsableNetwork;
 import edu.uci.ics.jung.algorithms.layout.Layout;
@@ -13,11 +16,16 @@ public class SVGExporter {
 
 	private BrowsableNetwork network;
 	private Layout< Vertex, Edge> layout;
+	private boolean isDirected = false;
 	
 	
 	public SVGExporter(BrowsableNetwork network, Layout<Vertex,Edge> layout) {
 		this.network = network;
 		this.layout = layout;
+		for(Edge e : network.getEdges() ) {
+			if(network.getSource(e) != null)
+				isDirected = true;
+		}
 	}
 	
 	public void toSVG(File file, int height, int width) {
@@ -47,7 +55,7 @@ public class SVGExporter {
 				for(Edge e : network.getIncidentEdges(n)) {
 					int sourceId, destId;
 					// is the network directed?
-					if(network.getSource(e) != null) {
+					if(isDirected) {
 						sourceId = index.get(network.getSource(e));
 						destId = index.get(network.getDest(e));
 					} else {
@@ -111,25 +119,59 @@ public class SVGExporter {
 			p.println("<body onload='init();'>");
 			p.println("<svg:svg width='"+ width +"px' height='" + height + "px'>");
 			p.println("  <svg:polyline points='0,0 " + width + ",0 " + width + "," + height + " 0," + height + "' style='stroke:black; fill:none;'/>");
+			// declare markers if the network is directed
+			Map< Integer, Set<Color> > markers;
+			Map< Color, Integer > colorId = null;
+			if(isDirected) {
+				markers = new HashMap<Integer, Set<Color> >();
+				colorId = new HashMap<Color, Integer>();
+				for(Edge e : network.getEdges() ) {
+					Integer refX = (int)(network.getDest(e).getSize()*8/10 + 10);
+					if(!markers.containsKey(refX)) {
+						markers.put(refX, new HashSet<Color>() );
+					}
+					if(!colorId.containsKey(e.getColor())) {
+						colorId.put(e.getColor(), colorId.size());
+					}
+					if(!markers.get(refX).contains(e.getColor())) {
+						markers.get(refX).add(e.getColor());
+						//output the marker
+						String markerColor = "" + e.getColor().getRed() + ',' + e.getColor().getGreen() + ',' + e.getColor().getBlue();
+						String markerName = "arrow_" + (int)network.getDest(e).getSize() + '_' + colorId.get(e.getColor());
+						p.println("   <svg:marker id='" + markerName + "' viewBox='0 0 10 10' refX='" + refX + "' refY='5' markerUnits='userSpaceOnUse' markerWidth='12' markerHeight='12' orient='auto' fill='rgb(" + markerColor + ")'>");
+						p.println("      <svg:path d='M 0 0 L 10 5 L 0 10 z'></svg:path>");
+						p.println("   </svg:marker>");
+					}
+					
+				}
+			}
 			for(Edge e : network.getEdges() ) {
 				Vertex sourceNode, destNode;
 				// is the network directed?
-				if(network.getSource(e) != null) {
+				if(isDirected) {
 					sourceNode = network.getSource(e);
-					destNode = network.getDest(e);
+					destNode = network.getDest(e);					
 				} else {
 					sourceNode = network.getEndpoints(e).getFirst();
 					destNode = network.getEndpoints(e).getSecond();
 				}
+				// we do not add self loops
+				//if(sourceNode == destNode) continue;
 				int sourceId = index.get(sourceNode);
 				int destId = index.get(destNode);
 				String edgeName = "edge" + (sourceId < destId? sourceId: destId) + (sourceId > destId? sourceId: destId);
-				double x1, y1, x2, y2;
+				int x1, y1, x2, y2;
 				x1 = (int)layout.transform(sourceNode).getX();
 				y1 = (int)layout.transform(sourceNode).getY();
 				x2 = (int)layout.transform(destNode).getX();
 				y2 = (int)layout.transform(destNode).getY();
-				p.println("  <svg:line id='" + edgeName + "' x1='" + x1 + "' y1='" + y1 + "' x2='" + x2 + "' y2='" + y2 + "' style='stroke:rgb(" + e.getColor().getRed() + "," + e.getColor().getGreen() + "," + e.getColor().getBlue() + ");stroke-width:" + e.getWidth() + "'/>");
+				p.print("  <svg:line id='" + edgeName + "' x1='" + x1 + "' y1='" + y1 + "' x2='" + x2 + "' y2='" + y2 + "' ");
+				if(isDirected) {
+					// specify the marker
+					String markerName = "arrow_" + (int)network.getDest(e).getSize() + '_' + colorId.get(e.getColor());
+					p.print("marker-end='url(#" + markerName + ")' ");
+				}
+				p.println("style='stroke:rgb(" + e.getColor().getRed() + "," + e.getColor().getGreen() + "," + e.getColor().getBlue() + ");stroke-width:" + e.getWidth() + "'/>");
 			}
 			for(Vertex n : network.getVertices() ) {
 				p.println("  <svg:circle id='" + index.get(n) + "' r='" + n.getSize() + "' cx='" + (int)layout.transform(n).getX() + "' cy='" +(int) layout.transform(n).getY() + "' style='fill:rgb(" + n.getFillColor().getRed() + "," + n.getFillColor().getGreen() + "," + n.getFillColor().getBlue() + "); stroke:rgb(" + n.getColor().getRed() + "," + n.getColor().getGreen() + "," + n.getColor().getBlue() + "); stroke-width:" + n.getWidth() + ";'/>");
